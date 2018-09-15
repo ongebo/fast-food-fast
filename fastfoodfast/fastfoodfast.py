2377c3b20d7c856b407671cfb152f5dc581298d3
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-15 17:00:27 +0300

    [#160474185] Update route to get specific order

--- fastfoodfast/fastfoodfast.py
+++ fastfoodfast/fastfoodfast.py
@@ -1,19 +1,16 @@
-from flask import Flask, session, jsonify, abort
-from .models import Order
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
-order_model = Order()
 
-@app.route('/api/v1/customer/orders/<int:id>')
-def get_specific_user_order(id):
-    if not session.get('customer_logged_in'):
-        abort(401)
-    return jsonify(order_model.read(id))
-
-
-@app.route('/api/v1/admin/orders/<int:id>')
+@app.route('/api/v1/orders/<int:id>')
 def get_specific_order(id):
-    if not session.get('admin_logged_in'):
-        abort(401)
-    return jsonify(order_model.read(id))
+    order = 'order' + str(id)
+    result = None
+    for order in orders:
+        if order['id'] == id:
+            result = order
+    return jsonify({order: result})
+
+
+orders = list()
