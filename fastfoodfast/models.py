9d55413426eb0879450e2fe71e8774eb15ef8296
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-16 19:08:21 +0300

    [#160474087] Define model method to create new order

--- fastfoodfast/models.py
+++ fastfoodfast/models.py
@@ -2,6 +2,10 @@ class OrderNotFound(Exception):
     pass
 
 
+class BadRequest(Exception):
+    pass
+
+
 class Order:
     orders = list()
 
@@ -15,3 +19,27 @@ class Order:
             if order['id'] == id:
                 return order
         raise OrderNotFound('No order with id {} exists'.format(id))
+    
+    def create_order(self, order):
+        try:
+            if 'items' in order:
+                for item in order['items']:
+                    assert 'item' in item and 'quantity' in item and 'cost' in item
+                new_order = dict()
+                total_cost = 0
+                for item in order['items']:
+                    total_cost += int(item['cost'])
+                order_id = None
+                if len(Order.orders) == 0:
+                    order_id = 0
+                else:
+                    order_id = Order.orders[-1]['order-id'] + 1
+                new_order['order-id'] = order_id
+                new_order['items'] = order['items']
+                new_order['total-cost'] = total_cost
+                Order.orders.append(new_order)
+                return new_order
+            else:
+                raise BadRequest
+        except:
+            raise
