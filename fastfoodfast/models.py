d19e8e74c1a1fefdeac2fcb964ecdfd0e8c846b8
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-16 20:49:08 +0300

    [#160474232] Add model functionality to update order status

--- fastfoodfast/models.py
+++ fastfoodfast/models.py
@@ -44,3 +44,44 @@ class Order:
                 raise BadRequest
         except:
             raise
+    
+    def update_order_status(self, id, new_order):
+        order_to_update = self.get_order(id)
+        if self.validate_order(new_order):
+            order_to_update.update(new_order)
+        else:
+            raise BadRequest
+    
+    def validate_order_item(self, item):
+        try:
+            dict(item)
+        except:
+            return False
+        if 'item' not in item and type(item['item']) != str:
+            return False
+        elif 'quantity' not in item and not str(item['quantity']).isdigit():
+            return False
+        elif 'cost' not in item and not str(item['cost']).isdigit():
+            return False
+        elif len(item) > 3:
+            return False
+        else:
+            return True
+    
+    def validate_order(self, order):
+        try:
+            dict(order)
+            list(order['items'])
+        except:
+            return False
+        items_list_correct = True
+        for item in order['items']:
+            if not self.validate_order_item(item): items_list_correct = False
+        if not items_list_correct:
+            return False
+        elif 2 <= len(order) <= 3 and 'status' not in order and 'total-amount' not in order:
+            return False
+        elif len(order) > 3:
+            return False
+        else:
+            return True
