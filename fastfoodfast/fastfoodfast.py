eb1d4362b175d986db0b78a569b41315aea3d37d
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-16 16:54:20 +0300

    Reset the basic application files

---
+++ fastfoodfast/fastfoodfast.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/orders')
+def get_all_orders():
+    pass
+
+
+@app.route('/api/v1/orders/<int:id>')
+def get_a_specific_order(id):
+    pass
+
+
+@app.route('/api/v1/orders', methods=['POST'])
+def place_a_new_order():
+    pass
+
+
+@app.route('/api/v1/orders/<int:id>', methods=['PUT'])
+def update_order_status(id):
+    pass
