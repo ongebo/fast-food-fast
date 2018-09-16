4c8e588f92049a6762f602b2131442a711209822
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-16 16:42:55 +0300

    Clean out api branch

--- test_models.py
+++
@@ -1,8 +0,0 @@
-from fastfoodfast.models import FoodItem, food_items
-
-
-def test_food_item_creation():
-    assert len(food_items) == 0
-    item = FoodItem()
-    created_item = item.create_food_item('Hamburger', 'set', 5000)
-    assert created_item == {'id': 0, 'item': 'Hamburger', 'unit': 'set', 'rate': 5000}
