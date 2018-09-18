c6cb88e969c038c5088879a4c1f7657d504407e9
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-18 18:04:23 +0300

    Test that the API correctly adds an order to the orders list [#160546296]

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -1,10 +1,15 @@
 import pytest
 from fastfoodfast import app
+from fastfoodfast.models import Order
 
 
 @pytest.fixture
 def test_client():
     return app.test_client()
+
+
+def reset_orders_list():
+    Order.orders = list()
 
 
 def test_index_page_contains_welcome_message(test_client):
@@ -17,3 +22,17 @@ def test_api_returns_empty_orders_list_when_no_order_has_been_created(test_clien
     data = response.get_json()
     assert 'orders' in data and data['orders'] == []
     assert response.status_code == 200
+
+
+def test_api_correctly_creates_a_new_order(test_client):
+    order_items = [
+        {'item': 'hamburger', 'quantity': 2, 'cost': 12000},
+        {'item': 'pizza', 'quantity': 1, 'cost': 12000}
+    ]
+    response = test_client.post('/api/v1/orders', json={'items': order_items})
+    data = response.get_json()
+    assert 'items' in data and data['items'] == order_items
+    assert 'status' in data and data['status'] == 'pending'
+    assert 'total-cost' in data and data['total-cost'] == 24000
+    assert 'order-id' in data
+    reset_orders_list()
