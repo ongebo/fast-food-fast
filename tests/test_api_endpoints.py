9e939b2cb9b9523777e8cc43a792b85d971050e2
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-18 17:20:00 +0300

    Test that API returns empty list when no order was added [#160546296]

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -9,4 +9,11 @@ def test_client():
 
 def test_index_page_contains_welcome_message(test_client):
     response = test_client.get('/')
-    assert b'Welcome to Fast-Food-Fast!' in response.data
+    assert 'Welcome to Fast-Food-Fast!' in response.get_json()
+
+
+def test_api_returns_empty_orders_list_when_no_order_has_been_created(test_client):
+    response = test_client.get('/api/v1/orders')
+    data = response.get_json()
+    assert 'orders' in data and data['orders'] == []
+    assert response.status_code == 200
