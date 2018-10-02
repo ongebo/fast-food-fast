2ba50b55b13e6b3d320d2e048966f8872ad53dc6
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-02 23:32:07 +0300

    Test that API can place a new order for food [#160882892]

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -57,3 +57,40 @@ def test_api_returns_error_message_given_wrong_login_data(test_client):
     response = test_client.post('/api/v1/auth/login', json=invalid_data)
     assert response.status_code == 400
     assert 'error' in response.get_json()
+
+
+def test_api_can_place_an_order_for_food(test_client, connection):
+    user_data = {'username': 'Loki Odinson', 'password': 'mischief'}
+    response_1 = test_client.post('/api/v1/auth/signup', json=user_data)
+    assert response_1.status_code == 201
+    response_2 = test_client.post('/api/v1/auth/login', json=user_data)
+    data = response_2.get_json()
+    assert response_2.status_code == 201 and 'token' in data
+    order = {'items': [{'item': 'pizza', 'quantity': 1, 'cost': 20000}]}
+    headers = {'Authorization': 'Bearer ' + data['token']}
+    response_3 = test_client.post('/api/v1/users/orders', json=order, headers=headers)
+    assert response_3.status_code == 201
+    assert 'order-id' in response_3.get_json()
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM order_items WHERE item = %s', ('pizza', ))
+    cursor.execute('DELETE FROM orders WHERE customer = %s', ('Loki Odinson', ))
+    cursor.execute('DELETE FROM users WHERE username = %s', ('Loki Odinson', ))
+    connection.commit()
+    connection.close()
+
+
+def test_api_returns_error_message_given_incorrect_post_order_data(test_client, connection):
+    user_data = {'username': 'Loki Odinson', 'password': 'mischief'}
+    response_1 = test_client.post('/api/v1/auth/signup', json=user_data)
+    response_2 = test_client.post('/api/v1/auth/login', json=user_data)
+    data = response_2.get_json()
+    headers = {'Authorization': 'Bearer ' + data['token']}
+    response_3 = test_client.post('/api/v1/users/orders', json={}, headers=headers)
+    assert response_1.status_code == 201
+    assert response_2.status_code == 201
+    assert response_3.status_code == 400
+    assert b'invalid data' in response_3.data
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM users WHERE username = %s', ('Loki Odinson', ))
+    connection.commit()
+    connection.close()
