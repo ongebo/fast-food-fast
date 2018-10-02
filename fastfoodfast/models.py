41827a04c194868648cbf4efb54b9446380304b7
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-02 23:31:26 +0300

    Add model method for creating new order in database [#160882892]

--- fastfoodfast/models.py
+++ fastfoodfast/models.py
@@ -1,5 +1,5 @@
-import psycopg2
-from .validation import validate_user
+import psycopg2, uuid
+from .validation import validate_user, validate_order
 from werkzeug.security import generate_password_hash, check_password_hash
 
 expected_user_data_format = """
@@ -64,3 +64,51 @@ class User:
         user['username'] = result[0]
         user['password'] = result[1]
         return user
+
+
+class Order:
+    def __init__(self, db='fffdb'):
+        self.database = db
+    
+    def connect_to_db(self):
+        self.conn = psycopg2.connect(
+            database=self.database,
+            user='ongebo',
+            password='nothing',
+            host='127.0.0.1',
+            port='5432'
+        )
+        self.cursor = self.conn.cursor()
+
+    def create_order(self, order, customer):
+        """Adds a new order to the database"""
+        if validate_order(order):
+            new_order = dict()
+            new_order['items'] = order['items']
+            new_order['status'] = 'pending'
+            total_cost = 0
+            for item in order['items']:
+                total_cost += float(item['cost'])
+            new_order['total-cost'] = total_cost
+            order_id = str(uuid.uuid4())[:8]
+            new_order['order-id'] = order_id
+
+            self.connect_to_db()
+            self.cursor.execute(
+                "INSERT INTO orders (public_id, customer, status, total_cost) VALUES (%s, %s, %s, %s)",
+                (order_id, customer, 'pending', total_cost)
+            )
+            self.cursor.execute(
+                "SELECT id FROM orders WHERE public_id = '{}'".format(order_id)
+            )
+            primary_key = self.cursor.fetchone()[0]
+            for item in new_order['items']:
+                self.cursor.execute(
+                    'INSERT INTO order_items (order_id, item, quantity, cost) VALUES (%s, %s, %s, %s)',
+                    (primary_key, item['item'], item['quantity'], item['cost'])
+                )
+            self.conn.commit()
+            self.conn.close()
+            return new_order
+        else:
+            raise Exception
