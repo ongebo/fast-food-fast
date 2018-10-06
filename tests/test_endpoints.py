186d315ae85dced818409f970a2e5a53d7f0ca2c
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-07 00:17:13 +0300

    Make string arguments more readable and consistent

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -40,7 +40,7 @@ def test_api_correctly_registers_a_user(test_client, connection):
     assert data['username'] == 'John Doe'
     assert check_password_hash(data['password'], 'JonathanDoe')
     assert data['admin'] == False
-    connection.cursor().execute('DELETE FROM users WHERE username = \'John Doe\'')
+    connection.cursor().execute('DELETE FROM users WHERE username = %s', ('John Doe', ))
     connection.commit()
     connection.close()
 
@@ -59,7 +59,7 @@ def test_api_correctly_logs_in_registered_user(test_client, connection):
     response_2 = test_client.post('/api/v1/auth/login', json=user_data)
     assert response_2.status_code == 200
     assert 'token' in response_2.get_json()
-    connection.cursor().execute("DELETE FROM users WHERE username = 'Jon Snow'")
+    connection.cursor().execute('DELETE FROM users WHERE username = %s', ('Jon Snow', ))
     connection.commit()
     connection.close()
 
@@ -129,7 +129,7 @@ def test_api_returns_user_order_history(test_client, connection):
     cursor.execute('DELETE FROM order_items WHERE item = %s', ('hot dog', ))
     cursor.execute('DELETE FROM order_items WHERE item = %s', ('salad', ))
     cursor.execute('DELETE FROM orders WHERE customer = %s', ('steve rodgers', ))
-    cursor.execute("DELETE FROM orders WHERE customer = '{}'".format('steve rodgers'))
+    cursor.execute('DELETE FROM orders WHERE customer = %s', ('steve rodgers', ))
     connection.commit()
     connection.close()
 
