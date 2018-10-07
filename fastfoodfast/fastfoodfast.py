60accb0d532789b9c1dd43ec4d20c6a5a0f9a134
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-07 12:23:03 +0300

    Add module docstring

--- fastfoodfast/fastfoodfast.py
+++ fastfoodfast/fastfoodfast.py
@@ -1,3 +1,7 @@
+"""
+Definitions of API routes for managing users, orders, and food menu data.
+"""
+
 from flask import Flask, request, jsonify, Response
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
