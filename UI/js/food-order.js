8f11dd9ae0b580a598d7dbbed7e46aac99f72fd4
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-25 23:11:33 +0300

    Organize accordion code into one function [#161299938]

--- UI/js/food-order.js
+++ UI/js/food-order.js
@@ -1,14 +1,15 @@
-var acc = document.getElementsByClassName("menu-item");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("selected");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block"){
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+function attachEventHandlersToMenuItems() {
+    var acc = document.getElementsByClassName("menu-item");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("selected");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block"){
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
     }
-  });
 }
