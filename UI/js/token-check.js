0a3687859dfd6148d5b388e47164eb73efc53e2b
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-27 00:34:49 +0300

    Update code to redirect user to index.html incase token has expired

--- UI/js/token-check.js
+++ UI/js/token-check.js
@@ -1,25 +1,24 @@
 var token = sessionStorage.getItem("token");
-if (token == null || tokenHasExpired(token))
+if (token == null) {
     window.location.href = "index.html"; // redirect to login page
+}
+redirectIfTokenHasExpired(token);
 
-function tokenHasExpired(token) {
+async function redirectIfTokenHasExpired(token) {
     var headers = new Headers();
     headers.append("Content-Type", "application/json");
     headers.append("Authorization", "Bearer " + token);
     var request = new Request(
-        "https://gbo-fff-with-db.herokuapp.com/api/v1/users/orders",
-        {headers: headers}
+        "https://gbo-fff-with-db.herokuapp.com/api/v1/menu",
+        {method: "GET", headers: headers}
     );
     
     try {
-        fetch(request).then(
-            res => {
-                if (res.status == 401)
-                    return true
-                else
-                    return false
-            }
-        );
+        var response = await fetch(request);
+        console.log(response.status);
+        if (response.status == 401) {
+            window.location.href = "index.html";
+        }
     } catch (error) {
         alert(error);
     }
