43a50924d4e2e6c907dba87ac279325213783b0a
Isaac Ongebo <isaacongebo@gmail.com>
2018-10-26 16:31:21 +0300

    Refactor fetchAndDisplayMenu() to reduce lines of code

--- UI/js/food-order.js
+++ UI/js/food-order.js
@@ -8,11 +8,7 @@ function fetchAndDisplayMenu() {
             response => {
                 return response.json();
             }
-        ).then(
-            menuData => {
-                displayMenu(menuData);
-            }
-        );
+        ).then(displayMenu);
     } catch (error) {
         alert(error);
     }
@@ -97,31 +93,36 @@ function attachEventHandlersToMenuItems() {
 
 function addItemToList(event) {
     var buttonId = event.target.id;
-    alert(buttonId.match(/[0-9]+/));
-    var orderItem = createOrderItem(buttonId.match(/[0-9]+/));
+    var idSuffix = buttonId.match(/[0-9]+/);
+    var orderItem = createOrderItem(idSuffix);
     orderList.push(orderItem);
-    updateDisplayedOrderList(orderItem);
+    var itemElement = document.querySelector("#item" + idSuffix);
+    var unit = itemElement.innerHTML.match(/per [a-zA-Z]+/).toString().substring(4);
+    updateDisplayedOrderList(orderItem, unit);
 }
 
 function createOrderItem(idSuffix) {
     var itemSelector = "#item" + idSuffix;
-    var item = document.getElementById("name" + idSuffix);
+    var item = document.getElementById("name" + idSuffix).innerHTML;
     var quantity = document.querySelector("#input" + idSuffix).value;
-    var rate = parseFloat(document.querySelector(itemSelector).innerHTML.match(/[0-9]+/));
+    var rate = document.querySelector(itemSelector).innerHTML.match(/ [0-9]+ /);
+    var rate = parseFloat(rate);
     var cost = quantity * rate;
     var orderItem = {item: item, quantity: quantity, cost: cost};
     return orderItem;
 }
 
-function updateDisplayedOrderList(orderItem) {
+function updateDisplayedOrderList(orderItem, unit) {
     var itemsList = document.querySelector(".order-list");
+    var orderButton = document.querySelector(".order-list > button");
     var emptyListMessage = document.getElementById("empty-list");
     if (emptyListMessage != null)
-        itemsList.removeChild(emptyListMessage);
-    var content = orderItem.quantity + " " + orderItem.unit + "s of @ Ugx " + orderItem.cost;
+        emptyListMessage.parentNode.removeChild(emptyListMessage);
+    var content = orderItem.quantity + " " + unit + "s of " + orderItem.item +
+    " @ Ugx " + orderItem.cost;
     var text = document.createTextNode(content);
     var p = document.createElement("p");
     p.setAttribute("class", "order-list-item");
     p.appendChild(text);
-    itemsList.appendChild(p);
+    itemsList.insertBefore(p, orderButton);
 }
