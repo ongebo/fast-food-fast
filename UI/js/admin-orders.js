d19a5cb9124d948a761121d007c87bd347ee30b3
Isaac Ongebo <isaacongebo@gmail.com>
2018-11-03 16:55:54 +0300

    Redirect admin to login page if token has expired [#161312000]

--- UI/js/admin-orders.js
+++ UI/js/admin-orders.js
@@ -1,7 +1,16 @@
 async function fetchAndDisplayOrders() {
     var request = createOrdersRequestObject();
     try {
-        //
+        var response = await fetch(request);
+        if (response.status == 200) {
+            // fetch and display response body
+        } else if (response.status == 401) {
+            window.location.href = "index.html";
+        } else if (response.status == 404) {
+            // display empty orders error
+        } else {
+            // unexpected error occurred, display message + status code
+        }
     } catch (error) {
         alert(error);
     }
